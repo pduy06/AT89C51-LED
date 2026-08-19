8e751642a2586f371cda93531f9f0083ee9aa96c
pduy06 <duypham1366@gmail.com>
2026-08-19 17:02:06 +0700

    Add LED blink delay

--- src/main.c
+++ src/main.c
@@ -1,10 +1,21 @@
 #include <REGX51.H>
 
+void delay_ms(unsigned int ms)
+{
+    unsigned int i, j;
+
+    for (i = 0; i < ms; i++)
+        for (j = 0; j < 123; j++);
+}
+
 void main(void)
 {
     while (1)
     {
         P2 = 0xFF;
+        delay_ms(500);
+
         P2 = 0x00;
+        delay_ms(500);
     }
 }
